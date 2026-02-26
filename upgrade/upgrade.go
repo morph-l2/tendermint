3692a2a2889c2030e1140a18404dbdeb8ae2674b
allen.wu <allen.wu@bitget.com>
2026-02-26 15:59:02 +0800

    add consensus switch flag

--- upgrade/upgrade.go
+++ upgrade/upgrade.go
@@ -2,13 +2,22 @@ package upgrade
 
 // Hardcoded upgrade parameters
 var (
-	// UpgradeBlockHeight is the block height at which the sequencer upgrade activates
-	// For testing, set this to a low value (e.g., 50)
-	// TODO: add sequencer update logic
-	UpgradeBlockHeight int64 = 10
+	// UpgradeBlockHeight is the block height at which the consensus switches to sequencer mode.
+	// Default is -1 (upgrade disabled). Set via --consensus.switchHeight flag.
+	// When < 0, the upgrade never activates.
+	UpgradeBlockHeight int64 = -1
 )
 
-// IsUpgraded returns true if the given height is at or after the upgrade height
+// IsUpgraded returns true if the given height is at or after the upgrade height.
+// Returns false when UpgradeBlockHeight < 0 (upgrade disabled).
 func IsUpgraded(height int64) bool {
+	if UpgradeBlockHeight < 0 {
+		return false
+	}
 	return height >= UpgradeBlockHeight
+}
+
+// SetUpgradeBlockHeight sets the upgrade block height.
+func SetUpgradeBlockHeight(height int64) {
+	UpgradeBlockHeight = height
 }
